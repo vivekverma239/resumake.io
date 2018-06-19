33f2da2c5ac41b39fed6118cd9e89581d8af919a
saadq <saad@saadq.com>
2018-06-19 01:16:58 -0400

    setup project

---
+++ client/src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+const App = () => (
+  <div>
+    <h1>Resumake</h1>
+  </div>
+)
+
+const root = document.getElementById('app')
+
+render(<App />, root)
